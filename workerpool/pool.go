0b484c71365024c408815415c6ac73b641008599
Anastasiia Nevodchikova <nd@kode.ru>
2025-01-16 20:31:18 +0300

    implements service with monitoring health services

---
+++ workerpool/pool.go
@@ -0,0 +1,78 @@
+package workerpool
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+type Job struct {
+	URL string
+}
+
+type Result struct {
+	URL string
+	StatusCode int
+	ResponseTime time.Duration
+	Error error
+}
+
+func (r Result) Info() string {
+	if r.Error != nil {
+		return fmt.Sprintf("[ERROR] - [%s] - %s", r.URL, r.Error.Error())
+	}
+	return fmt.Sprintf("[SUCCESS] - [%s] - Status: %d, ResponseTime: %s", r.URL, r.StatusCode, r.ResponseTime)
+}
+
+type Pool struct {
+	worker *worker
+	workersCount int
+
+	jobs chan Job
+	results chan Result
+
+	wg *sync.WaitGroup
+	isStopped bool
+}
+
+func New(workersCount int, timeout time.Duration, results chan Result) *Pool {
+	return &Pool{
+		worker: newWorker(timeout),
+		workersCount: workersCount,
+		jobs: make(chan Job),
+		results: results,
+		wg: new(sync.WaitGroup),
+	}
+}
+
+func (p *Pool) Init() {
+	for i := 0; i < p.workersCount; i++ {
+		go p.initWorker(i)
+	}
+}
+
+func (p *Pool) Push(job Job) {
+	if p.isStopped {
+		return
+	}
+
+	p.jobs <- job
+	p.wg.Add(1)
+}
+
+
+func (p *Pool) Stop() {
+	p.isStopped = true
+	close(p.jobs)
+	p.wg.Wait()
+}
+
+func (p *Pool) initWorker(id int) {
+	for job := range p.jobs {
+		p.results <- p.worker.process(job)
+		p.wg.Done()
+	}
+
+	log.Printf("[worker ID %d] finished processing", id)
+}
